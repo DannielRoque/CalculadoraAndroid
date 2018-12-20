05bbbf5cbef97150395aeb36693abb66f048eb4e
Daniel Roque <daniel.roque2@fatec.sp.gov.br>
2018-12-20 17:07:26 -0200

    Calculadora de bolso simples

---
+++ CalculaTec/app/src/main/java/com/example/daniel/calculatec/CalcuadoraActivity.java
@@ -0,0 +1,104 @@
+package com.example.daniel.calculatec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class CalcuadoraActivity extends AppCompatActivity {
+    private Calculadora calc;
+
+    private boolean usuarioEstaDigitandoUmNumero;
+    private boolean separadorDecimalFoiDigitado;
+    private TextView txtVisor;
+    private String separador;
+    private char separadorChar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calcuadora);
+
+        calc = new Calculadora();
+
+        usuarioEstaDigitandoUmNumero = false;
+
+        separadorDecimalFoiDigitado = false;
+
+        txtVisor = (TextView)findViewById(R.id.txtVisor);
+        txtVisor.setText("0");
+
+        Locale localizacao = getResources().getConfiguration().locale;
+        NumberFormat formatador = NumberFormat.getInstance(localizacao);
+        char separadorChar = ',';
+        if(formatador instanceof DecimalFormat){
+            DecimalFormatSymbols simbolo = ((DecimalFormat)formatador).getDecimalFormatSymbols();
+            separadorChar = simbolo.getDecimalSeparator();
+        }
+        separador = String.valueOf(separadorChar);
+        Button btnSeparador= (Button)findViewById(R.id.button19);
+        btnSeparador.setText(separador);
+    }
+
+    public void onClickNumeros(View v){
+        Button botaoTocado = (Button)v;
+        String digito = botaoTocado.getText().toString();
+
+        String textoNoVisor = txtVisor.getText().toString();
+
+        if(!usuarioEstaDigitandoUmNumero || textoNoVisor.equals("0")){
+            txtVisor.setText(digito);
+            if(!digito.equals("0")){
+                usuarioEstaDigitandoUmNumero=true;
+            }
+        }else{
+            txtVisor.setText(textoNoVisor + digito);
+        }
+    }
+
+    public void onClickOperacoes(View v){
+        Button botaoTocado = (Button)v;
+        String operacao = botaoTocado.getText().toString();
+
+        if(operacao.equals(separador) && !separadorDecimalFoiDigitado){
+            separadorDecimalFoiDigitado=true;
+            if (!usuarioEstaDigitandoUmNumero)
+                txtVisor.setText("0" + separador);
+            else
+                txtVisor.setText(txtVisor.getText().toString()+ separador);
+            usuarioEstaDigitandoUmNumero = true;
+        }else if(!operacao.equals(separador)){
+            String valorSemVirgula=txtVisor.getText().toString().replace(separadorChar,'.');
+
+            calc.setOperando(Double.parseDouble(valorSemVirgula));
+            calc.realizarOperacao(operacao);
+
+            String textoResultado = String.valueOf(calc.getOperando());
+
+            if(textoResultado.endsWith(".0")){
+                textoResultado = textoResultado.substring(0, textoResultado.length() -2);
+            }
+            txtVisor.setText(textoResultado.replace('.',separadorChar));
+            usuarioEstaDigitandoUmNumero = false;
+            separadorDecimalFoiDigitado = false;
+        }
+    }
+    public void onClickMemoria(View v){
+        Button botaoTocado = (Button)v;
+        String operacaoMemoria = botaoTocado.getText().toString();
+
+        String valorSemVirgula = txtVisor.getText().toString().replace(separadorChar,'.');
+
+        calc.setOperando(Double.parseDouble(valorSemVirgula));
+        calc.realizarOperacaoDeMemoria(operacaoMemoria);
+
+        usuarioEstaDigitandoUmNumero = false;
+    }
+
+}
